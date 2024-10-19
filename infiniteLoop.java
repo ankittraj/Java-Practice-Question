8f4c492a6569fd6c7af365b37d8ea4e8ddab4041
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-10-19 23:47:53 +0530

    Add files via upload

---
+++ infiniteLoop.java
@@ -0,0 +1,11 @@
+//Write an infinite loop using do while condition.
+
+public class infiniteLoop {
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("This is an infinite loop");
+            
+        } while (true); // The condition 'true' makes the loop run forever
+    }
+}

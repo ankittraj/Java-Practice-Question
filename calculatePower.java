40a1cb87112deeff3c6a6d76d20016e87ad986e3
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-10-23 23:49:34 +0530

    Add files via upload

---
+++ calculatePower.java
@@ -0,0 +1,33 @@
+// public class calculatePower {
+//  	public static void main(String[] args) {
+	    
+//  	double base = 1.5;
+//      double expo1 = 2.5;
+//      double expo2 = -2.5;
+//      double res1, res2;
+    
+//      // calculates the power
+//      res1 = Math.pow(base, expo1);
+//      res2 = Math.pow(base, expo2);
+//  	System.out.println(base + " ^ " + expo1 + " = " + res1 );
+//  	System.out.println(base + " ^ " + expo2 + " = " + res2 );
+//  	}
+// }
+
+public class calculatePower
+{
+	public static void main(String[] args) {
+	    
+	double base = 1.5;
+    // Works only when exponent is positive integer
+    int expo = 2;
+    double res = 1.0;
+    
+    while (expo != 0) {
+        res *= base;
+        expo--;
+    }
+    
+    System.out.println("Result = " + res); 
+	}
+}

e42c52cfce7036a1940575019ea90a0128a2c202
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-10-29 22:09:24 +0530

    Add files via upload

---
+++ Hellocalculator.java
@@ -0,0 +1,32 @@
+// Calculator using simple arithmetic operator with switch case
+import java.util.*;
+public class Hellocalculator{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter a :");
+        int a = sc.nextInt();
+        System.out.println("enter b :");
+        int b = sc.nextInt();
+        System.out.println("enter operator :");
+        char operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+' : System.out.println(a+b);
+            break;
+
+            case '-' : System.out.println(a-b);
+            break;
+
+            case '*' : System.out.println(a*b);
+            break;
+
+            case '/' : System.out.println(a/b);
+            break;
+
+            case '%' : System.out.println(a%b);
+            break;
+
+            default : System.out.println("wrong operator");
+        }
+    }
+}
